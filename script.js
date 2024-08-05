c3f2271756eec34c2885ec182fa85ec89d0c4d14
tnail45 <tjnail2000@gmail.com>
2024-08-05 10:06:29 -0700

    Make function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+
+
+
+
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 10);
+    if (x <= 3) {
+        console.log("Rock")
+    }
+    else if (x <= 6) {
+        console.log("Paper")
+    }
+    else {
+        console.log("Scissors")
+    }
+
+}
+
+getComputerChoice();
