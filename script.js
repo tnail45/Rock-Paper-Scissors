cf4ebb831d4c68f0883906906a483ec52bf8a49a
tnail45 <tjnail2000@gmail.com>
2024-08-05 13:15:19 -0700

    Create playRound function

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
 
 
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 10);
-    if (x <= 3) {
-        console.log("Rock")
+    if (x <=3) {
+        return "rock";
     }
     else if (x <= 6) {
-        console.log("Paper")
+        return "paper"
     }
     else {
-        console.log("Scissors")
+        return "scissors"
     }
-
 }
-
 
 function getHumanChoice () {
     let entry = prompt("Rock, Paper, or Scissors?")
-    entry = entry.toLowerCase();
-    if (entry == "rock") {
-        console.log("rock")
-    }
-    else if (entry == "paper") {
-        console.log("paper")
-    }
-    else if (entry == "scissors") {
-        console.log("scissors")
-    }
-    else {
-        console.log("Invalid entry!")
-    }
+    return entry;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice();
+    if ((humanChoice == "rock") && (computerChoice == "rock")) {
+        return alert("Tie!");
+    }
+    else if ((humanChoice == "rock") && (computerChoice == "paper")) {
+        return alert("Computer wins!", computerScore++ )
+    }
+    else if ((humanChoice == "rock") && (computerChoice == "scissors")) {
+        return alert("You win!", humanScore++)
+    }
+    else if ((humanChoice == "paper") && (computerChoice == "rock")) {
+        return alert("You win!", humanScore++)
+    }
+    else if ((humanChoice == "paper") && (computerChoice == "paper")) {
+        return alert("Tie!")
+    }
+    else if ((humanChoice == "paper") && (computerChoice == "scissors")) {
+        return alert("computer wins!", computerScore++)
+    }
+    else if ((humanChoice == "scissors") && (computerChoice == "rock")) {
+        return alert("computer wins!", computerScore++)
+    }
+    else if ((humanChoice == "scissors") && (computerChoice == "paper")) {
+        return alert("You win!", humanScore++)
+    }
+    else if ((humanChoice == "scissors") && (computerChoice == "scissors")) {
+        return alert("Tie!")
+    }
+}
+playRound();
+console.log(computerScore);
+console.log(humanScore);
